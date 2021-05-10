94b19905eb88b6021ad0e401ebff07d8d7749337
Dan Ross <daniellross@gmail.com>
2021-05-10 15:58:01 -0500

    added leaveEvent function to handle deleting gamervent asociation. add ternary on event list to toggle button

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react"
 import { EventContext } from "./EventProvider.js"
 
 export const EventList = (props) => {
-    const { events, getEvents, joinEvent } = useContext(EventContext)
+    const { events, getEvents, joinEvent, leaveEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -12,7 +12,7 @@ export const EventList = (props) => {
         <article className="events">
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
-                <button 
+                <button
                     className="btn btn-2 btn-primary"
                     onClick={() => props.history.push('/events/create')}>Create New Event</button>
             </header>
@@ -25,19 +25,24 @@ export const EventList = (props) => {
                         <div>
                             {
                                 new Date(event.date).toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
+                                    {
+                                        weekday: 'long',
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric'
+                                    })
                             }
                             @ {event.time}
                         </div>
-                        <button className="btn btn-2"
-                                onClick={() => joinEvent(event.id)}>
-                                    Join
-                                </button>
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id)}
+                                >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id)}
+                                >Join</button>
+                        }
                     </section>
                 })
             }
--- src/components/event/EventProvider.js
+++ src/components/event/EventProvider.js
@@ -3,22 +3,22 @@ import React, { useState } from "react"
 export const EventContext = React.createContext()
 
 export const EventProvider = (props) => {
-    const [ events, setEvents ] = useState([])
+    const [events, setEvents] = useState([])
 
     const getEvents = () => {
         return fetch("http://localhost:8000/events", {
-            headers:{
+            headers: {
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`
             }
         })
             .then(response => response.json())
             .then(setEvents)
     }
-    
+
     const createEvent = newEvent => {
         return fetch("http://localhost:8000/events", {
             method: "POST",
-            headers:{
+            headers: {
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`,
                 "Content-Type": "application/json"
             },
@@ -28,19 +28,31 @@ export const EventProvider = (props) => {
     }
 
     const joinEvent = eventId => {
-        return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+        return fetch(`http://localhost:8000/events/${eventId}/signup`, {
             method: "POST",
-            headers:{
+            headers: {
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`,
                 "Content-Type": "application/json"
             }
         })
             .then(response => response.json())
+            .then(getEvents)
+    }
+
+    const leaveEvent = eventId => {
+        return fetch(`http://localhost:8000/events/${eventId}/signup`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+                "Content-Type": "application/json"
+            }
+        })
+            .then(getEvents)
     }
 
     return (
-        <EventContext.Provider value={{ events, getEvents, createEvent, joinEvent }} >
-            { props.children }
+        <EventContext.Provider value={{ events, getEvents, createEvent, joinEvent, leaveEvent }} >
+            { props.children}
         </EventContext.Provider>
     )
 }
